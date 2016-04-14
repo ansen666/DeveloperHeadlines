f8f0940ba1ba87fee610aa30ab4107765ff0564b
安辉 anhui <privateworkemail@163.com>
2016-04-15 02:10:23 +0800

    开始做启动页面

---
+++ src/com/ansen/developerheadlines/activity/LauncherActivity.java
@@ -0,0 +1,18 @@
+package com.ansen.developerheadlines.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+/**
+ * 第一次启动页面
+ * @author Ansen
+ *
+ */
+public class LauncherActivity extends FragmentActivity{
+	@Override
+	protected void onCreate(Bundle bundle) {
+		super.onCreate(bundle);
+	}
+	
+}
