77543ce8c906da6cf42feb20961cdc664af5a29b
ansen <anhui@365yf.com>
2016-05-05 13:49:54 +0800

    进行修改

--- app/src/main/java/com/ansen/developerheadlines/fragment/MainFragment.java
+++ app/src/main/java/com/ansen/developerheadlines/fragment/MainFragment.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -17,6 +18,8 @@ import com.ansen.developerheadlines.adapter.FragmentAdapter;
 
 public class MainFragment extends Fragment{
 	private ViewPager vPager;
+	private AppBarLayout appBarLayout;
+	private SelectedFragment selectedFragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class MainFragment extends Fragment{
 
 		FragmentAdapter pagerAdapter = new FragmentAdapter(getActivity().getSupportFragmentManager());
 
-		SelectedFragment selectedFragment=new SelectedFragment();
+		selectedFragment=new SelectedFragment();
 		SubscribeFragment subscribeFragment=new SubscribeFragment();
 		FindFragment findFragment=new FindFragment();
 
@@ -46,6 +49,19 @@ public class MainFragment extends Fragment{
 		toolbar.setNavigationIcon(R.mipmap.ic_menu_white);
 		toolbar.setTitle("关注公众号[Android开发者666]");
 		toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+		appBarLayout= (AppBarLayout) rootView.findViewById(R.id.appBarLayout);
+		appBarLayout.addOnOffsetChangedListener(onOffsetChangedListener);
 		return rootView;
     }
+
+
+	private AppBarLayout.OnOffsetChangedListener onOffsetChangedListener=new AppBarLayout.OnOffsetChangedListener() {
+		@Override
+		public void onOffsetChanged(AppBarLayout appBarLayout, int i){
+			//i>=0  Toolbar全部显示
+			selectedFragment.setPullRefresh(i>=0);
+			System.out.println("i值:"+i);
+		}
+	};
 }
